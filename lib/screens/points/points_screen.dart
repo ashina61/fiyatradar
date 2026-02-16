f5c9625cadf550b3f52b3186632581e2e8bb8ab7
ashina61 <fan61atik@gmail.com>
2026-02-16 21:24:19 +0300

    Fix points screen build syntax error

--- lib/screens/points/points_screen.dart
+++ lib/screens/points/points_screen.dart
@@ -132,7 +132,6 @@ class _PointsScreenState extends ConsumerState<PointsScreen> {
                   },
                         ),
                       ),
-                    ),
                   );
                 },
               ),
