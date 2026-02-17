d7e9e69febb7331498756b1032bd2d72acf77f6f
ashina61 <fan61atik@gmail.com>
2026-02-17 14:02:32 +0300

    Fix bracket mismatch in product detail price card

--- lib/screens/product/product_detail_screen.dart
+++ lib/screens/product/product_detail_screen.dart
@@ -1226,7 +1226,7 @@ class _ProductDetailScreenState extends ConsumerState<ProductDetailScreen> {
                 child: _buildLastPriceContributorRow(latestPrice),
               ),
             ],
-          ),
+          ],
         ),
       ),
     );
