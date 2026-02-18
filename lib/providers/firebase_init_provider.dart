ad31c50ec0a6301b39b292d15fd661c8cac0642f
ashina61 <fan61atik@gmail.com>
2026-02-18 11:03:15 +0300

    Fix Dart string literals and firebase init provider

--- lib/providers/firebase_init_provider.dart
+++ lib/providers/firebase_init_provider.dart
@@ -1,18 +1,13 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final firebaseInitializedListenableProvider = ValueListenableProvider<bool>(
-  (ref) => firebaseInitializedNotifier,
-);
+bool _firebaseInitialized = false;
 
 final firebaseInitializedProvider = Provider<bool>((ref) {
-  return ref.watch(firebaseInitializedListenableProvider);
+  return _firebaseInitialized;
 });
 
-final ValueNotifier<bool> firebaseInitializedNotifier = ValueNotifier(false);
-
-bool get firebaseInitialized => firebaseInitializedNotifier.value;
+bool get firebaseInitialized => _firebaseInitialized;
 
 set firebaseInitialized(bool value) {
-  firebaseInitializedNotifier.value = value;
+  _firebaseInitialized = value;
 }
