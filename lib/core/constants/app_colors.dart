798062a0f708205d319274da6f93300335d23050
ashina61 <fan61atik@gmail.com>
2026-04-03 15:51:11 +0300

    Add missing textSubtle color constant

--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -7,6 +7,7 @@ class AppColors {
   static const Color tan = Color(0xFFC4A57B);
   static const Color textPrimary = Color(0xFFF4E7D9);
   static const Color textSecondary = Color(0xFFC7B7A6);
+  static const Color textSubtle = Color(0xFF9E8F82);
   static const Color success = Color(0xFF8A9A8B);
   static const Color warning = Color(0xFFD2A66C);
   static const Color danger = Color(0xFFC77772);
